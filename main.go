a706dce1ee8f09c827166d631df930e18afeb84f
Adrian Cybulski <adrian@cybulski.cc>
2021-09-10 10:37:37 +0100

    Add auth middleware options to cli

--- main.go
+++ main.go
@@ -14,11 +14,6 @@ var (
 )
 
 func main() {
-	e.Use(middleware.Recover())
-	e.Use(middleware.Logger())
-	e.Use(AuthMiddleware())
-
-	e.Any("*", AnyAuth)
 
 	app.Command("serve", "start http server", cmdServe)
 
@@ -27,10 +22,24 @@ func main() {
 
 func cmdServe(cmd *cli.Cmd) {
 	var (
-		addr = cmd.StringOpt("addr", ":8080", "bind address")
+		addr    = cmd.StringOpt("addr", ":8080", "bind address")
+		minUID  = cmd.IntOpt("minUID", 1000, "skips users below UID limit")
+		minGID  = cmd.IntOpt("minGID", 1000, "skips users below GID limit")
+		exclude = cmd.StringsOpt("exculeUsers", []string{"root"}, "exclude usernames")
 	)
 
 	cmd.Action = func() {
+		e.Use(middleware.Recover())
+		e.Use(middleware.Logger())
+
+		e.Use(AuthMiddlewareWithConfig(
+			PAMAuthConfig{
+				*minUID, *minGID, *exclude,
+			},
+		))
+
+		e.Any("*", AnyAuth)
+
 		e.Logger.Info(e.Start(*addr))
 	}
 }
